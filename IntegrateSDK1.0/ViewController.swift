a967a0ecbc7f2c1ccfa0e2dcfe0ee8f32b25c0a1
Ta Minh Tu <tutm@davidta.local>
2018-07-29 22:34:17 +0700

    Initial Commit

---
+++ IntegrateSDK1.0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IntegrateSDK1.0
+//
+//  Created by Ta Minh Tu on 7/29/18.
+//  Copyright © 2018 David Ta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
